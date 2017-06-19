54ae7365b1be8f1bb8225df3fcd923396ccb26fa
Telianth <stole17@hotmail.com>
2017-06-19 18:43:19 +0200

    changed main view background color

--- src/components/Router.js
+++ src/components/Router.js
@@ -8,7 +8,7 @@ import BurgerPlace from './BurgerPlace';
 const RouterComponent = () => (
 
         <Router
-         sceneStyle={{ paddingTop: 50 }}
+         sceneStyle={{ paddingTop: 50, backgroundColor: '#d3d3d3' }}
          navigationBarStyle={{ backgroundColor: '#d3d3d3' }}
          titleStyle={{ color: '#181817' }}
         >
